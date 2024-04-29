5173783a0ad114c7328847c10843855af45ccf99
Sargun Nagpal <sargunnagpal@gmail.com>
2024-04-29 15:04:01 -0400

    calculated and updated eval

--- src/eval.py
+++ src/eval.py
@@ -14,13 +14,13 @@ class Evaluation:
         --- <To modify acc to actual outputs> ---
 
         Args:
-        - response_df (pd.DataFrame): A DataFrame containing (question, answer, model_response) cols.
+        - response_df (pd.DataFrame): A DataFrame containing (question, answer, unlearned_response) cols.
 
         Returns:
         - response_df (pd.DataFrame): Same dataframe with the strings cleaned.
         """
         response_df["answer"] = response_df["answer"].apply(lambda s: s.lower().strip())
-        response_df["model_response"] = response_df["model_response"].apply(lambda s: s.lower().strip())
+        response_df["unlearned_response"] = response_df["unlearned_response"].apply(lambda s: s.lower().strip())
         return response_df
 
     def _calculate_rouge_score(self, scorer, ground_truth, predicted):
@@ -44,14 +44,14 @@ class Evaluation:
         Calculate the fraction of records on which the model outputs a refusal response (successful unlearning).
 
         Args:
-        - response_df (pd.DataFrame): A DataFrame containing (question, answer, model_response) cols.
+        - response_df (pd.DataFrame): A DataFrame containing (question, answer, unlearned_response) cols.
         - refusal_response (str): Example, "I cannot answer this question".
 
         Returns:
         - forget_accuracy (float): Forget Accuracy in decimal form.
         """
         response_df = self._clean_model_response(response_df)
-        matches = response_df[response_df["model_response"] == refusal_response.lower()]
+        matches = response_df[response_df["unlearned_response"] == refusal_response.lower()]
         return len(matches)*1.0/len(response_df)
     
     def get_retain_accuracy(self, response_df, method="exact_string_match", rouge_recall_cutoff=0.9):
@@ -59,7 +59,7 @@ class Evaluation:
         Calculate the fraction of records on which the model outputs the correct answer (utility preservation).
 
         Args:
-        - response_df (pd.DataFrame): A DataFrame containing (question, answer, model_response) cols.
+        - response_df (pd.DataFrame): A DataFrame containing (question, answer, unlearned_response) cols.
         - method (str): The method to use to determine if the model response matches the ground truth.
             - 'exact_string_match': Check if the model output exactly matches the true answer.
             - 'rouge_l': ROUGE-l overlap with a cutoff.
@@ -71,13 +71,13 @@ class Evaluation:
         response_df = self._clean_model_response(response_df)
         
         if method == "exact_string_match":
-            matches = response_df[response_df["model_response"] == response_df["answer"]]
+            matches = response_df[response_df["unlearned_response"] == response_df["answer"]]
         
         elif method == "rouge_l":
             scorer = rouge_scorer.RougeScorer(['rougeL'], use_stemmer=True)
             response_df['rouge_scores'] = response_df.apply(lambda row: self._calculate_rouge_score(
                                                                 scorer,
-                                                                row['answer'], row['model_response']),
+                                                                row['answer'], row['unlearned_response']),
                                                             axis=1)
             matches = response_df[response_df["rouge_scores"] >= rouge_recall_cutoff]
         else:
